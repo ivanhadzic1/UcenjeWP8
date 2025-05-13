92fe1d8095232796ddf22482c4f8606bfdb535e6
ivanhadzic1 <ivan.hadzic07@gmail.com>
2025-05-13 18:43:25 +0200

    2.lauf

--- skriptawp8.sql
+++ skriptawp8.sql
@@ -1,32 +1,38 @@
-﻿--create database edunovawp8;
+﻿use master;
+go
+drop database if exists edunovawp8;
+go
+create database edunovawp8;
+go
+use edunovawp8;
 
 --drop database edunovawp8;
 
 --use edunovawp8;
 
---create table smjerovi(
---sifra int,
---naziv varchar(50),
---cijena decimal(18,2),
---datumpokretanja datetime,
---aktivan bit
---);
+create table smjerovi(
+sifra int not null primary key identity(1,1),
+naziv varchar(50) not null,
+cijena decimal(18,2) null,
+datumpokretanja datetime null,
+aktivan bit not null default 0
+);
 
---create table grupe(
---sifra int,
---naziv varchar(20),
---smjer int,
---predavac varchar(50)
---);
+create table grupe(
+sifra int not null primary key identity(1,1),
+naziv varchar(20) not null,
+smjer int not null,
+predavac varchar(50)
+);
 
---create table polaznici(
---sifra int,
---ime varchar(50),
---prezime varchar(50),
---email varchar(100)
---);
+create table polaznici(
+sifra int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(50) not null,
+email varchar(100) null
+);
 
---create table clanovi(
---grupa int,
---polaznik int
---);
+create table clanovi(
+grupa int not null,
+polaznik int not null
+);
