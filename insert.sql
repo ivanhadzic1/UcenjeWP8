9b8a66f40ba6428e997c446fd0cd28e9b0ee9aed
ivanhadzic1 <ivan.hadzic07@gmail.com>
2025-05-16 18:02:36 +0200

    rad ma unosu podataka

---
+++ insert.sql
@@ -0,0 +1,14 @@
+﻿use edunovawp8;
+
+select * from smjerovi;
+
+insert into smjerovi (naziv, cijena, datumpokretanja, aktivan)
+values ('Web programiranje', 1200.54, '2025-05-16 17:00:01', 1);
+
+insert into smjerovi (naziv)
+values ('Serviser'),('Web dizajn'),('Marketing');
+
+select * from grupe
+
+insert into grupe (naziv,smjer)
+values ('WP8',1), ('WP7',1), ('S1',2);
